716766066b6faf024405c2b560390a4d435822a9
Vincent Tian <vincenttian16@gmail.com>
2014-08-21 14:14:27 -0700

    add try catch to restaurant location

--- gift/ActivitySuggestorController.m
+++ gift/ActivitySuggestorController.m
@@ -13,40 +13,40 @@
 
 @interface ActivitySuggestorController ()
 
-    @property(strong, nonatomic) IBOutlet UILabel *name1;
-    @property(strong, nonatomic) IBOutlet UILabel *category1;
-    @property(strong, nonatomic) IBOutlet UILabel *rating1;
-    @property(strong, nonatomic) IBOutlet UILabel *reviews1;
-    @property(strong, nonatomic) IBOutlet UILabel *address1;
-    @property(strong, nonatomic) IBOutlet UILabel *url1;
+@property(strong, nonatomic) IBOutlet UILabel *name1;
+@property(strong, nonatomic) IBOutlet UILabel *category1;
+@property(strong, nonatomic) IBOutlet UILabel *rating1;
+@property(strong, nonatomic) IBOutlet UILabel *reviews1;
+@property(strong, nonatomic) IBOutlet UILabel *address1;
+@property(strong, nonatomic) IBOutlet UILabel *url1;
 
-    @property(strong, nonatomic) IBOutlet UILabel *name2;
-    @property(strong, nonatomic) IBOutlet UILabel *category2;
-    @property(strong, nonatomic) IBOutlet UILabel *rating2;
-    @property(strong, nonatomic) IBOutlet UILabel *reviews2;
-    @property(strong, nonatomic) IBOutlet UILabel *address2;
-    @property(strong, nonatomic) IBOutlet UILabel *url2;
+@property(strong, nonatomic) IBOutlet UILabel *name2;
+@property(strong, nonatomic) IBOutlet UILabel *category2;
+@property(strong, nonatomic) IBOutlet UILabel *rating2;
+@property(strong, nonatomic) IBOutlet UILabel *reviews2;
+@property(strong, nonatomic) IBOutlet UILabel *address2;
+@property(strong, nonatomic) IBOutlet UILabel *url2;
 
-    @property(strong, nonatomic) IBOutlet UILabel *name3;
-    @property(strong, nonatomic) IBOutlet UILabel *category3;
-    @property(strong, nonatomic) IBOutlet UILabel *rating3;
-    @property(strong, nonatomic) IBOutlet UILabel *reviews3;
-    @property(strong, nonatomic) IBOutlet UILabel *address3;
-    @property(strong, nonatomic) IBOutlet UILabel *url3;
+@property(strong, nonatomic) IBOutlet UILabel *name3;
+@property(strong, nonatomic) IBOutlet UILabel *category3;
+@property(strong, nonatomic) IBOutlet UILabel *rating3;
+@property(strong, nonatomic) IBOutlet UILabel *reviews3;
+@property(strong, nonatomic) IBOutlet UILabel *address3;
+@property(strong, nonatomic) IBOutlet UILabel *url3;
 
-    @property(strong, nonatomic) IBOutlet UILabel *name4;
-    @property(strong, nonatomic) IBOutlet UILabel *category4;
-    @property(strong, nonatomic) IBOutlet UILabel *rating4;
-    @property(strong, nonatomic) IBOutlet UILabel *reviews4;
-    @property(strong, nonatomic) IBOutlet UILabel *address4;
-    @property(strong, nonatomic) IBOutlet UILabel *url4;
+@property(strong, nonatomic) IBOutlet UILabel *name4;
+@property(strong, nonatomic) IBOutlet UILabel *category4;
+@property(strong, nonatomic) IBOutlet UILabel *rating4;
+@property(strong, nonatomic) IBOutlet UILabel *reviews4;
+@property(strong, nonatomic) IBOutlet UILabel *address4;
+@property(strong, nonatomic) IBOutlet UILabel *url4;
 
-    @property(strong, nonatomic) IBOutlet UILabel *name5;
-    @property(strong, nonatomic) IBOutlet UILabel *category5;
-    @property(strong, nonatomic) IBOutlet UILabel *rating5;
-    @property(strong, nonatomic) IBOutlet UILabel *reviews5;
-    @property(strong, nonatomic) IBOutlet UILabel *address5;
-    @property(strong, nonatomic) IBOutlet UILabel *url5;
+@property(strong, nonatomic) IBOutlet UILabel *name5;
+@property(strong, nonatomic) IBOutlet UILabel *category5;
+@property(strong, nonatomic) IBOutlet UILabel *rating5;
+@property(strong, nonatomic) IBOutlet UILabel *reviews5;
+@property(strong, nonatomic) IBOutlet UILabel *address5;
+@property(strong, nonatomic) IBOutlet UILabel *url5;
 
 @end
 
@@ -85,7 +85,13 @@
                  // business1
                  NSString *name1 = [[results objectAtIndex:0] objectForKey:@"name"];
                  NSString *rating1 = [[results objectAtIndex:0] objectForKey:@"rating"];
-                 NSString *address1 = [[[[results objectAtIndex:0] objectForKey:@"location"] objectForKey:@"address"] objectAtIndex:0];
+                 NSString *address1 = @"";
+                 @try {
+                     address1 = [[[[results objectAtIndex:0] objectForKey:@"location"] objectForKey:@"address"] objectAtIndex:0];
+                 }
+                 @catch (NSException * e) {
+                     address1 = @"No given address";
+                 }
                  NSString *reviews1 = [[results objectAtIndex:0] objectForKey:@"review_count"];
                  NSString *url1 = [[results objectAtIndex:0] objectForKey:@"url"];
                  NSString *category1 = [[[[results objectAtIndex:0] objectForKey:@"categories"] objectAtIndex:0] objectAtIndex:0];
@@ -94,7 +100,13 @@
                  // business2
                  NSString *name2 = [[results objectAtIndex:1] objectForKey:@"name"];
                  NSString *rating2 = [[results objectAtIndex:1] objectForKey:@"rating"];
-                 NSString *address2 = [[[[results objectAtIndex:1] objectForKey:@"location"] objectForKey:@"address"] objectAtIndex:0];
+                 NSString *address2 = @"";
+                 @try {
+                     address2 = [[[[results objectAtIndex:1] objectForKey:@"location"] objectForKey:@"address"] objectAtIndex:0];
+                 }
+                 @catch (NSException * e) {
+                     address2 = @"No given address";
+                 }
                  NSString *reviews2 = [[results objectAtIndex:1] objectForKey:@"review_count"];
                  NSString *url2 = [[results objectAtIndex:1] objectForKey:@"url"];
                  NSString *category2 = [[[[results objectAtIndex:1] objectForKey:@"categories"] objectAtIndex:0] objectAtIndex:0];
@@ -103,7 +115,13 @@
                  // business3
                  NSString *name3 = [[results objectAtIndex:2] objectForKey:@"name"];
                  NSString *rating3 = [[results objectAtIndex:2] objectForKey:@"rating"];
-                 NSString *address3 = [[[[results objectAtIndex:2] objectForKey:@"location"] objectForKey:@"address"] objectAtIndex:0];
+                 NSString *address3 = @"";
+                 @try {
+                     address3 = [[[[results objectAtIndex:2] objectForKey:@"location"] objectForKey:@"address"] objectAtIndex:0];
+                 }
+                 @catch (NSException * e) {
+                     address3 = @"No given address";
+                 }
                  NSString *reviews3 = [[results objectAtIndex:2] objectForKey:@"review_count"];
                  NSString *url3 = [[results objectAtIndex:2] objectForKey:@"url"];
                  NSString *category3 = [[[[results objectAtIndex:2] objectForKey:@"categories"] objectAtIndex:0] objectAtIndex:0];
@@ -113,7 +131,13 @@
                  // business4
                  NSString *name4 = [[results objectAtIndex:3] objectForKey:@"name"];
                  NSString *rating4 = [[results objectAtIndex:3] objectForKey:@"rating"];
-                 NSString *address4 = [[[[results objectAtIndex:3] objectForKey:@"location"] objectForKey:@"address"] objectAtIndex:0];
+                 NSString *address4 = @"";
+                 @try {
+                     address4 = [[[[results objectAtIndex:3] objectForKey:@"location"] objectForKey:@"address"] objectAtIndex:0];
+                 }
+                 @catch (NSException * e) {
+                     address4 = @"No given address";
+                 }
                  NSString *reviews4 = [[results objectAtIndex:3] objectForKey:@"review_count"];
                  NSString *url4 = [[results objectAtIndex:3] objectForKey:@"url"];
                  NSString *category4 = [[[[results objectAtIndex:3] objectForKey:@"categories"] objectAtIndex:0] objectAtIndex:0];
@@ -123,7 +147,13 @@
                  // business5
                  NSString *name5 = [[results objectAtIndex:4] objectForKey:@"name"];
                  NSString *rating5 = [[results objectAtIndex:4] objectForKey:@"rating"];
-                 NSString *address5 = [[[[results objectAtIndex:4] objectForKey:@"location"] objectForKey:@"address"] objectAtIndex:0];
+                 NSString *address5 = @"";
+                 @try {
+                     address5 = [[[[results objectAtIndex:4] objectForKey:@"location"] objectForKey:@"address"] objectAtIndex:0];
+                 }
+                 @catch (NSException * e) {
+                     address5 = @"No given address";
+                 }
                  NSString *reviews5 = [[results objectAtIndex:4] objectForKey:@"review_count"];
                  NSString *url5 = [[results objectAtIndex:4] objectForKey:@"url"];
                  NSString *category5 = [[[[results objectAtIndex:4] objectForKey:@"categories"] objectAtIndex:0] objectAtIndex:0];
@@ -204,13 +234,13 @@
     self.url5.text = [biz5 objectForKey: @"url5"];
 }
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
